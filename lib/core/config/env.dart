3bb912c9d332dfa0c5e8aa72747be901c83380b3
arya <arya@aryas-MacBook-Air.local>
2025-10-28 20:42:58 +0700

    fix: name app, icon, and spplashscreen deplay

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -1,5 +1,5 @@
 class Env {
-  static const apiBase = String.fromEnvironment('API_BASE', defaultValue: 'http://10.0.2.2:8000');
+  static const apiBase = String.fromEnvironment('API_BASE', defaultValue: 'http://192.168.0.153:8000/api');
   static const env = String.fromEnvironment('ENV', defaultValue: 'dev');
   static const logLevel = String.fromEnvironment('LOG_LEVEL', defaultValue: 'debug');
 }
