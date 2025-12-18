caabe12901edf2d725b83fe4bd5b002664958425
Docs <arya@MacBook-Pro.local>
2025-12-18 22:20:41 +0700

    style(shop): replace card shadow with thin grey outline
    - Changed product card decoration from boxShadow to border
    - Added grey.shade300 border for cleaner look

--- lib/features/shop/presentation/pages/shop_page.dart
+++ lib/features/shop/presentation/pages/shop_page.dart
@@ -233,13 +233,7 @@ class _ShopPageState extends ConsumerState<ShopPage> {
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(12),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.05),
-              blurRadius: 10,
-              offset: const Offset(0, 4),
-            ),
-          ],
+          border: Border.all(color: Colors.grey.shade300),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
