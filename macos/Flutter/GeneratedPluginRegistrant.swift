45d3dd2024d6f7a876588df76368b8f306fb1fda
Docs <arya@MacBook-Pro.local>
2026-03-05 05:17:43 +0700

    chore: update dependencies and configure apk output name
    - Ran flutter pub upgrade to update minor package dependencies.
    - Renamed output APK to prenava.apk in android build config.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import file_selector_macos
 import flutter_secure_storage_macos
 import isar_flutter_libs
-import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 
@@ -16,7 +15,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
